e7617b0aff9daeb0d3b34377d99b30b11433fec4
Christiand666 <56047476+Christiand666@users.noreply.github.com>
2022-03-26 22:41:15 +0100

    opsætning, router til apien, opsætning af  filer osc.

--- foreningsblognetfrontend/src/main.js
+++ foreningsblognetfrontend/src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './Router';
+import Axios from 'axios';
+import VueAxios from 'vue-axios';
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+const app = createApp(app)
+app.config.globalProperties.hostname="https://localhost:7182"
+app.use(router)
+app.use(VueAxios, Axios)
+app.mount('#app')
+createApp(App).mount('#app') 
