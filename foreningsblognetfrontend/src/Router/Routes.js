1963ff89e48027797a309d4d7964e20eb0c72174
Thomas Leo Blok <37294981+thomasleoblok@users.noreply.github.com>
2022-04-28 22:42:35 +0200

    create event

--- foreningsblognetfrontend/src/Router/Routes.js
+++ foreningsblognetfrontend/src/Router/Routes.js
@@ -1,6 +1,7 @@
 const {createRouter, createWebHistory } = require("vue-router");
 import Login from "../components/Login.vue"
 import Events from "../components/Events.vue"
+import CreateEvent from "../components/CreateEvent.vue"
 import Register from "../components/Register.vue"
 import Home from "../components/Home.vue";
 
@@ -28,6 +29,11 @@ const routes = [
       path: "/events",
       name:"Events",
       component: Events,
+    },
+    {
+      path: "/events/create",
+      name:"CreateEvent",
+      component: CreateEvent,
     },
     {
       path: "/404",
