ccd13823cc210c4895683434bfc5cd50b75f5ab4
Christiand666 <56047476+Christiand666@users.noreply.github.com>
2022-05-10 22:10:03 +0200

    jj

--- foreningsblognetfrontend/src/components/Login/Register.vue
+++ foreningsblognetfrontend/src/components/Login/Register.vue
@@ -2,12 +2,12 @@
     <div class="container">
         <div class="card">
             <div class="card-body">
-                <h1>Sign Up</h1>
-                <p>Please fill in this form to create an account.</p>
+                <h1>skriv dig op!</h1>
+                <p>Udfyld tingene tak.</p>
                 <hr>
 
-                <label for="username"><b>Username</b></label>
-                <input v-model="user.username" ref="username" type="text" placeholder="Enter Username" name="username" />
+                <label for="FullName"><b>FullName</b></label>
+                <input v-model="user.FullName" ref="FullName" type="text" placeholder="Enter FullName" name="FullName" />
                 
                 <label for="email"><b>Email</b></label>
                 <input v-model="user.email" ref="email" type="email" placeholder="Enter Email" name="email" />
@@ -18,15 +18,11 @@
                 <label for="psw-repeat"><b>Repeat Password</b></label>
                 <input v-model="user.repeatPassword" type="password" placeholder="Repeat Password" name="psw-repeat" />
                 
-                <label>
-                    <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px" /> Remember me
-                </label>
-                
-                <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+                <p>Big chunkes <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
 
                 <div class="clearfix">
-                    <button type="button" class="cancelbtn" v-on:click="back">Cancel</button>
-                    <button type="submit" class="signupbtn" v-on:click="signup">Sign Up</button>
+                    <button type="button" class="cancelbtn" v-on:click="back">Nevermind</button>
+                    <button type="submit" class="signupbtn" v-on:click="signup">Skriv mig op</button>
                 </div>
             </div>
         </div>
@@ -34,18 +30,17 @@
 </template>
 
 <script>
-import axios from 'axios';
+
 import Swal from 'sweetalert2';
 
 export default({
     data(){
         return{
             user:{
-                userId:0,
-                username:"",
                 email:"",
                 password:"",
-                repeatPassword:""
+                repeatPassword:"",
+                Fullname:"",
             },
         }
     },
@@ -56,30 +51,12 @@ export default({
         back(){
             this.$router.push({ name: 'Login' });
         },
-        signup(){
-            if(this.checkValidation()){
-            axios.post(this.hostname + "/api/user/registration", this.user)
-                .then(response => {
-                    if (response.data.userId > 0) {
-                         Swal.fire("Successfully registered")
-                        .then(() => {
-                            this.back();
-                        });
-                    }else{
-                        Swal.fire("Error : Something went wrong.");
-                    }
-                })
-                .catch(error => {
-                    if (error.response) {
-                        Swal.fire(error.response.data);
-                    }
-                });
-            }
-        },
+        
+        
         checkValidation(){
-            if(!this.user.username){
-                this.$refs.username.focus();
-                Swal.fire("Give username !");
+            if(!this.user.Fullname){
+                this.$refs.FullName.focus();
+                Swal.fire("Give FullName !");
                 return;
             }
             if(!this.user.email){
